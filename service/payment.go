5e5656c78cfdcaf07d547a7dd4cc7df26b9a4247
axadjonovsardorbek <axadjonovsardorbeck@gmail.com>
2024-06-12 14:04:32 +0500

    fixed some files

--- service/payment.go
+++ service/payment.go
@@ -1,17 +1,51 @@
 package service
 
 import (
-	r "payment-service/genproto/payment"
+	"context"
+	p "payment-service/genproto/payment"
 	st "payment-service/storage/postgres"
 )
 
 type PaymentService struct {
 	storage st.Storage
-	r.UnimplementedPaymentServiceServer
+	p.UnimplementedPaymentServiceServer
 }
 
 func NewPaymentService(storage *st.Storage) *PaymentService {
 	return &PaymentService{
 		storage: *storage,
 	}
+}
+
+func (s *PaymentService) Create(ctx context.Context, payment *p.PaymentCreateReq)(*p.PaymentGetByIdResp,error){
+	
+	resp, err := s.storage.PaymentS.Create(payment)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *PaymentService) GetById(ctx context.Context, id *p.PaymentGetByIdReq)(*p.PaymentGetByIdResp, error){
+	
+	payment, err := s.storage.PaymentS.GetById(id)
+
+	if err != nil{
+		return nil, err
+	}
+	
+	return payment, nil
+}
+
+func (s *PaymentService) Update(ctx context.Context, payment *p.PaymentUpdateReq)(*p.PaymentGetByIdResp, error){
+	
+	resp, err := s.storage.PaymentS.Update(payment)
+
+	if err != nil{
+		return nil, err
+	}
+	
+	return resp, nil
 }
--- storage/postgres/payment.go
+++ storage/postgres/payment.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"database/sql"
 	"payment-service/config/logger"
+	p "payment-service/genproto/payment"
 )
 
 type PaymentRepo struct {
@@ -12,4 +13,16 @@ type PaymentRepo struct {
 
 func NewPaymentRepo(db *sql.DB, logger *logger.Logger) *PaymentRepo {
 	return &PaymentRepo{db: db, Logger: logger}
+}
+
+func (pr *PaymentRepo) Create(payment *p.PaymentCreateReq)(*p.PaymentGetByIdResp,error){
+	return nil, nil
+}
+
+func (pr *PaymentRepo) GetById(id *p.PaymentGetByIdReq)(*p.PaymentGetByIdResp, error){
+	return nil, nil
+}
+
+func (pr *PaymentRepo) Update(payment *p.PaymentUpdateReq)(*p.PaymentGetByIdResp, error){
+	return nil, nil
 }
